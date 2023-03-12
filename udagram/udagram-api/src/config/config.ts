b2701644986ca41f8517a68bbd63b126b6ebad24
MaiAhmed18 <60844600+MaiAhmed18@users.noreply.github.com>
2023-03-12 21:55:18 +0200

    test

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -13,6 +13,8 @@ export const config = {
   host: process.env.POSTGRES_HOST,
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
+  accessKeyId : process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey : process.env.AWS_SECRET_ACCESS_KEY,
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
   jwt: {
