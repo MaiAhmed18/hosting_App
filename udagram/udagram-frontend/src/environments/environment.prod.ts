a51a3e0335e993fe282ec223a540c1f01dfd419f
MaiAhmed18 <60844600+MaiAhmed18@users.noreply.github.com>
2023-03-13 19:03:57 +0200

    http

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: '//udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
